ba223fb6a8a954a268820a06ca66d86d70d0b07b
manuel <manuel@danielcastelao.org>
2021-02-02 11:38:28 +0100

    Exemplo TreeView con BD. Modificación e engadidura de novos datos o Treeview

--- gtkTreeviewBDFiltrado.py
+++ gtkTreeviewBDFiltrado.py
@@ -42,12 +42,25 @@ class ExemploGtkTreeViewBdFiltrado (Gtk.Window):
         filtro_usuarios.set_visible_func (self.filtro_usuarios_sexo)
 
         trvDatosUsuarios = Gtk.TreeView(model = filtro_usuarios)
+        seleccion = trvDatosUsuarios.get_selection()
+        seleccion.connect("changed",self.on_trvDatosUsuarios_selection_changed)
 
 
-        for i, tituloColumna in enumerate (["Dni", "Nome", "Direccion","Edade","Sexo"]):
+        for i, tituloColumna in enumerate (["Dni", "Nome", "Direccion","Edade", "Sexo"]):
             celda = Gtk.CellRendererText()
             columna = Gtk.TreeViewColumn (tituloColumna, celda, text=i)
+            celda.props.editable = True
+            celda.connect("edited", self.on_celda_edited, i, modelo)
             trvDatosUsuarios.append_column(columna)
+
+        """celda = Gtk.CellRendererCombo()
+
+        celda.props.model.append ("Home")
+        celda.props.model.append ("Muller")
+        columna = Gtk.TreeViewColumn ("Sexo", celda, text=4)
+        celda.connect ("changed", self.on_celda_changed, modelo, 4)
+"""
+
 
         caixaV.pack_start(trvDatosUsuarios,True,True, 0)
 
@@ -60,8 +73,29 @@ class ExemploGtkTreeViewBdFiltrado (Gtk.Window):
         caixaH.pack_start (self.chkMuller, False, False, 2)
         caixaV.pack_start (caixaH, True, True, 0)
 
+        caixaH = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
+        self.txtDni = Gtk.Entry ()
+        self.txtNome = Gtk.Entry()
+        self.txtDireccion = Gtk.Entry()
+        self.txtEdade = Gtk.Entry()
+        self.cmbSexo = Gtk.ComboBoxText()
+        self.cmbSexo.append_text ("Home")
+        self.cmbSexo.append_text ("Muller")
+        caixaH.pack_start(self.txtDni, True, False, 2)
+        caixaH.pack_start(self.txtNome, True, False, 2)
+        caixaH.pack_start(self.txtDireccion, True, False, 2)
+        caixaH.pack_start(self.txtEdade, True, False, 2)
+        caixaH.pack_start(self.cmbSexo, True, False, 2)
+        caixaV.pack_start (caixaH,True, True, 0)
 
-        
+        caixaH = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
+        btnModificar = Gtk.Button (label = "Modificar")
+        btnModificar.connect ("clicked", self.on_btnModificar_clicked, seleccion)
+        caixaH.pack_end(btnModificar, False, False, 2)
+        btnEngadir = Gtk.Button (label = "Engadir")
+        btnEngadir.connect ("clicked", self.on_btnEngadir_clicked, modelo)
+        caixaH.pack_end(btnEngadir, False, False, 2)
+        caixaV.pack_end(caixaH, False, False, 2)
 
         self.add (caixaV)
 
@@ -83,9 +117,91 @@ class ExemploGtkTreeViewBdFiltrado (Gtk.Window):
             self.filtrado_sexo = "None"
         modelo.refilter()
 
+    def on_celda_edited (self, celda, fila, texto, columna, modelo):
+        modelo [fila][columna] = texto
 
+    def on_celda_changed(self, combo, fila, elemento, modelo, columna):
+        modelo [fila][columna] = combo.get_model()[elemento]
 
+    def on_trvDatosUsuarios_selection_changed(self, selection):
+        modelo, fila = selection.get_selected()
+        if fila  is not None:
+            self.txtDni.set_text(modelo [fila][0])
+            self.txtNome.set_text(modelo[fila][1])
+            self.txtDireccion.set_text(modelo [fila][2])
+            self.txtEdade.set_text (str(modelo [fila][3]))
+            for i,elemento in enumerate (self.cmbSexo.get_model()):
+                if elemento[0] == modelo [fila][4]:
+                    self.cmbSexo.set_active(i)
 
+    def on_btnModificar_clicked(self, boton, seleccion):
+        modelo, fila = seleccion.get_selected()
+        if fila  is not None:
+            modelo [fila][0] = self.txtDni.get_text()
+            modelo [fila][1] = self.txtNome.get_text()
+            modelo[fila][2] = self.txtDireccion.get_text()
+            modelo [fila][3] = int(self.txtEdade.get_text())
+            modelo [fila][4] = self.cmbSexo.get_active_text()
+
+            try:
+                bbdd = dbapi.connect("baseDatosTreeView.dat")
+            except dbapi.StandardError as e:
+                print(e)
+            else:
+                print("Base de datos aberta")
+            try:
+                cursor = bbdd.cursor()
+                cursor.execute("UPDATE usuarios SET nome=?, direccion=?, edade=?, sexo=? WHERE dni=?", (modelo [fila][1],
+                                                                                                        modelo [fila][2],
+                                                                                                        modelo [fila][3],
+                                                                                                        modelo [fila][4],
+                                                                                                        modelo [fila][0]))
+                bbdd.commit()
+
+            except dbapi.DatabaseError as e:
+                print("Erro facendo a consulta: " + str(e))
+            else:
+                print("Consulta executada")
+            finally:
+                cursor.close()
+                bbdd.close()
+
+            self.txtDni.set_text("")
+            self.txtNome.set_text("")
+            self.txtDireccion.set_text("")
+            self.txtEdade.set_text("")
+
+    def on_btnEngadir_clicked (self, boton, modelo):
+        rexistro =  (self.txtDni.get_text(),
+                          self.txtNome.get_text(),
+                          self.txtDireccion.get_text(),
+                          int(self.txtEdade.get_text()),
+                          self.cmbSexo.get_active_text())
+        modelo.append(rexistro)
+
+        try:
+            bbdd = dbapi.connect("baseDatosTreeView.dat")
+        except dbapi.StandardError as e:
+            print(e)
+        else:
+            print("Base de datos aberta")
+        try:
+            cursor = bbdd.cursor()
+            cursor.execute("INSERT INTO usuarios VALUES (?,?,?,?,?)", (rexistro))
+            bbdd.commit()
+
+        except dbapi.DatabaseError as e:
+            print("Erro facendo a consulta: " + str(e))
+        else:
+            print("Consulta executada")
+        finally:
+            cursor.close()
+            bbdd.close()
+
+        self.txtDni.set_text("")
+        self.txtNome.set_text("")
+        self.txtDireccion.set_text("")
+        self.txtEdade.set_text("")
 
 if __name__ == "__main__":
     ExemploGtkTreeViewBdFiltrado ()
@@ -113,85 +229,3 @@ if __name__ == "__main__":
 
 
 
-
-#print (dbapi.apilevel)
-#print (dbapi.threadsafety)
-#print (dbapi.paramstyle)
-
-try:
-    bbdd = dbapi.connect ("baseDatos.dat")
-    print (bbdd)
-
-
-except dbapi.StandardError as e:
-    print (e)
-else:
-    print ("Base de datos aberta")
-
-
-try:
-    cursor = bbdd.cursor ()
-    print (cursor)
-
-except dbapi.Error as e:
-    print (e)
-else:
-    print ("Cursor preparado")
-
-
-
-#try:
-    #cursor.execute (""" create table usuarios (dni text,
-     #                                          nome text,
-      #                                         direccion text)""")
-
-    #cursor.execute ("""insert into usuarios
-     #                 values ('3333-A', 'Maria', 'Canceleiro')""")
-    #cursor.execute ("""insert into usuarios
-     #                  values ('4444-B', 'Manuel', 'Rosalia')""")
-    #cursor.execute ("""insert into usuarios
-     #                  values ('5555-C', 'Ana', 'Areal')""")
-    #bbdd.commit()
-
-#except dbapi.DatabaseError as e:
-#    print ("Erro insertando os datos: " + str(e))
-#else:
- #   print ("Base de datos creada")
-  #  bbdd.close()
-
-
-try:
-    cursor.execute ("select * from usuarios")
-
-    #fetchone a seguinte tupla
-    #fetchall devolta un obxecto iterable con todalas tuplas
-    #fetcmany numero de tuplas pasado por parametro
-
-    for fila in cursor.fetchall():
-        #print (fila)
-        print ( "Nome: "+ fila [1])
-        print ( "DNI: " + fila [0])
-        print ( "Direccion: " + fila[2])
-
-except dbapi.DatabaseError as e:
-    print("Erro facendo a consulta: " + str(e))
-else:
-    print("Consulta executada")
-
-
-dni = input ("Introduce o dni")
-
-try:
-    consulta = "select * from usuarios where dni = ?"
-    print (consulta)
-    cursor.execute(consulta, (dni,))
-    for rexistro in cursor.fetchall():
-        print (rexistro)
-
-except dbapi.DatabaseError as e:
-    print("Erro facendo a consulta: " + str(e))
-else:
-    print("Consulta executada")
-finally:
-    cursor.close()
-    bbdd.close()
